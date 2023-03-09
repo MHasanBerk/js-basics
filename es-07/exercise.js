2f8bec52c7e4b8b6ddaab6833281af0e8ce1ea7b
Hasan Berk <hasnberk@outlook.com>
2023-03-09 19:56:35 +0300

    exercise 7 done

--- es-07/exercise.js
+++ es-07/exercise.js
@@ -1,3 +1,9 @@
-let hello = 'Ciao';     // number
-let age = 18;       // boolean
-let isGraduated = false;     // string
+let hello = Number('Ciao');     // number
+let age = Boolean(18);       // boolean
+let isGraduated = false.toString();     // string
+
+console.log(hello);
+console.log(age);
+console.log(isGraduated);
+
+//when we call hello variable, it gives NaN. Which means not a number. It's only natural because we are converting a string to a number.
