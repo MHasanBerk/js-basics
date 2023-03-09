6b4cbc65c87d3c57946a89187310df105adca7c1
Hasan Berk <hasnberk@outlook.com>
2023-03-09 21:35:21 +0300

    es 20 done

---
+++ es-20/exercise.js
@@ -0,0 +1,9 @@
+let primitive = "hello";
+
+if(typeof(primitive) == "number"){
+    console.log("type of the variable is number");
+}else if(typeof(primitive) == "string"){
+    console.log("type of the variable is string");
+}else if(typeof(primitive) == "boolean"){
+    console.log("type of the variable is boolean");
+}
