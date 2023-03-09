7bef805990bc16e96c860a87f477fcf70398d3b4
Hasan Berk <hasnberk@outlook.com>
2023-03-09 20:22:49 +0300

    exercise 10 done

--- es-10/exercise.js
+++ es-10/exercise.js
@@ -3,3 +3,8 @@ const yearsCompleted = 1;
 
 yearsCompleted == firstYearCompleted
 yearsCompleted === firstYearCompleted
+
+/* double equals compares values while triple equals compares values and types of the variables. So in this example 
+true == 1 returns true because their value are the same
+true === 1 returns false because while their value are the same, their type are not the same.
+*/
