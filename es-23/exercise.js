42569d0630940342cc3b66c5dd9bf3c9eecde580
Hasan Berk <hasnberk@outlook.com>
2023-03-09 21:51:21 +0300

    es 23 done

---
+++ es-23/exercise.js
@@ -0,0 +1,15 @@
+let price = 80;
+
+switch (price){
+    case 50:
+        console.log("Basic Room");
+        break;
+    case 80:
+        console.log("Junior Suite Room");
+        break;
+    case 100:
+        console.log("Master Suite");
+        break;
+    default:
+        console.log("There are no rooms for that price.");
+}
